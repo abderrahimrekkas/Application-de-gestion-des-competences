ec7348f26b135921285cb739e7be6622a3b50f24
abderrahimrekkas <rekkasabderrahim8@gmail.com>
2025-07-20 21:44:42 +0200

    mon code de backend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ mongoose
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
-      console.log(` Server running on http://localhost:${PORT}`);
+      console.log(` Server is running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
